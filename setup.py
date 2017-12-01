a5bb8885cf8467333068c74210412393e2e9e033
Joshua Klein <mobiusklein@gmail.com>
2017-12-01 17:23:31 -0500

    added support for compiling without Cython

--- setup.py
+++ setup.py
@@ -1,27 +1,40 @@
 import os
 import platform
 from setuptools import setup, Extension, find_packages
-from Cython.Distutils import build_ext
 
 import numpy as np
 
 extra_compile_args = []
-pltf = platform.system()
-if pltf == 'Windows':
+if platform.system().lower() == 'windows':
     extra_compile_args = ['/EHsc']
 
 
-ext_modules = [
-    Extension(
-        "pynumpress.pynumpress",
-        [
-            os.path.join('pynumpress/pynumpress.pyx'),
-            os.path.join('pynumpress/MSNumpress.cpp'),
-        ],
-        language='c++',
-        extra_compile_args=extra_compile_args,
-    )
-]
+try:
+    from Cython.Build import cythonize
+    ext_modules = [
+        Extension(
+            "pynumpress.pynumpress", [
+                os.path.join('pynumpress/pynumpress.pyx'),
+                os.path.join('pynumpress/MSNumpress.cpp'),
+            ],
+            language='c++',
+            extra_compile_args=extra_compile_args,
+            include_dirs=[np.get_include()]
+        )
+    ]
+    ext_modules = cythonize(ext_modules)
+except ImportError:
+    ext_modules = [
+        Extension(
+            "pynumpress.pynumpress", [
+                os.path.join('pynumpress/pynumpress.cpp'),
+                os.path.join('pynumpress/MSNumpress.cpp'),
+            ],
+            language='c++',
+            extra_compile_args=extra_compile_args,
+            include_dirs=[np.get_include()]
+        )
+    ]
 
 
 setup(
@@ -29,5 +42,4 @@ setup(
     packages=find_packages(),
     version='0.0.1',
     include_dirs=[np.get_include()],
-    ext_modules=ext_modules,
-    cmdclass={'build_ext': build_ext},)
+    ext_modules=ext_modules)
